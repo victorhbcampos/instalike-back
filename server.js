0b38015498b5f855bf6be0a0cc5cbbea3a9d6516
Victor Hugo Borges Campos <victor.hugoborcas18@gmail.com>
2024-11-19 09:41:32 -0300

    aula 02
    conclusão da segunda aula

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Paisagem deslumbrante!",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Meu pet é o mais fofo!",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à Imersão");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex( (post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
